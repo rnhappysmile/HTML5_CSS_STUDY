af4241c0f701e3afae6e1b858c91978a6feecc94
rnhappysmile <rnhappysmile@gmail.com>
2019-07-31 23:59:52 +0900

    mychart.js: x축 카테고리 등록 piechart.js, horizentalbar.js: 파일만 생성

--- 3rd_party/C3/mychart.js
+++ 3rd_party/C3/mychart.js
@@ -1,9 +1,20 @@
+var data1 = ['data1', 300000, 200000, 100000, 400000, 150, 250];
+var data2 = ['data2', 500000, 200000, 100000, 400000, 15, 25];
+var xName = ['x', 'abc1', 'abc2', 'abc3', 'abc4', 'abc5', 'abc6'];
+
 var chart = c3.generate({
     bindto: '#linechart',
     data: {
+        x: 'x',
         columns: [
-            ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 50, 20, 10, 40, 15, 25]
+            xName,
+            data1,
+            data2
         ]
-    }
+    },
+    axis: {
+        x: {
+            type: 'category' // this needed to load string x value
+        }
+    },
 });
